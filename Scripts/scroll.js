f4551fd239504ea00fb40067e1be297981d3083a
Zachariah Kline <zdkline@gmail.com>
2023-10-11 09:34:01 -0500

    Added new style sheets and reorganized html

--- Scripts/scroll.js
+++ Scripts/scroll.js
@@ -9,28 +9,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
             if (targetElement) {
                 // Adjust the duration to control the total scroll time (in milliseconds)
-                const scrollDuration = 400; // 1 second
+                const scrollDuration = 500; // ms
                 const startPosition = window.scrollY;
-                const targetPosition = targetElement.offsetTop;
+                const targetPosition = targetElement.getBoundingClientRect().top + startPosition;
                 const distance = targetPosition - startPosition;
                 const startTime = performance.now();
 
-                function cubicBezierEase(t, b, c, d) {
-                    const p0 = 0;
-                    const p1 = 0.42; // Adjust these control points as needed
-                    const p2 = 0.58; // Adjust these control points as needed
-                    const p3 = 1;
-                
-                    const t2 = t * t;
-                    const t3 = t2 * t;
-                
-                    return b + c * (p0 * (1 - t3) + 3 * p1 * t2 * (1 - t) + 3 * p2 * t * t2 + p3 * t3);
+                function cubicBezierEase(t) {
+                    return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
                 }
 
                 function scrollStep(timestamp) {
                     const currentTime = timestamp - startTime;
                     const progress = Math.min(currentTime / scrollDuration, 1);
-                    const easedProgress = cubicBezierEase(progress, 0, 1, 1);
+                    const easedProgress = cubicBezierEase(progress);
                     window.scrollTo(0, startPosition + distance * easedProgress);
 
                     if (progress < 1) {
