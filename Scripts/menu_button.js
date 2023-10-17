8d551cd9de68f316c5b9d852c2e1ec77e20a8bd8
Zachariah Kline <zdkline@gmail.com>
2023-10-17 14:28:59 -0500

    Edited the moble format

--- Scripts/menu_button.js
+++ Scripts/menu_button.js
@@ -1,4 +1,3 @@
-// JavaScript function to toggle the navigation menu
 document.addEventListener('DOMContentLoaded', function () {
     // JavaScript function to toggle the navigation menu
     function toggleMenu() {
@@ -6,9 +5,20 @@ document.addEventListener('DOMContentLoaded', function () {
         nav.classList.toggle('show-menu');
     }
 
+    function hideMenu() {
+        var nav = document.querySelector('nav ul');
+        nav.classList.remove('show-menu');
+    }
+
     // Add an event listener to the button
     var menuButton = document.getElementById('menu-button');
     if (menuButton) {
         menuButton.addEventListener('click', toggleMenu);
     }
+
+    // Add event listeners to each menu item to hide the menu when a selection is made
+    var menuItems = document.querySelectorAll('nav ul li a');
+    menuItems.forEach(function (item) {
+        item.addEventListener('click', hideMenu);
+    });
 });
